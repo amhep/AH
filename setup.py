f02e79fb01a00448e6eb75b2122ca99d1b8fc9b9
Ammon Hepworth <ammon.hepworth@gmail.com>
2014-06-13 16:27:23 -0600

    Added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='AH',
+    version='1.0',
+    description='OpenShift App',
+    author='Ammon Hepworth',
+    author_email='example@example.com',
+    url='http://cv-ammonhepworth.rhcloud.com/',
+    install_requires=['Django<=1.6'],
+)
